46688d58c8be1ceb1923b71a1d5b6da7cf308a6d
msseock <msseock@gmail.com>
2024-05-25 15:51:32 +0900

    👷 Build: iOS 프로젝트 초기 빌드

---
+++ KoApp-iOS/KoApp-iOS/KoApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoApp_iOSApp.swift
+//  KoApp-iOS
+//
+//  Created by 석민솔 on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct KoApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

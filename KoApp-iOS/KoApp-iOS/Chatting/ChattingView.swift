2007d5b8e8cd58a0aecb8c807e34a44a3b3db33f
msseock <msseock@gmail.com>
2024-06-08 01:10:15 +0900

    ✨ Feat: 텍스트 입력 -> Apple Speech STT로 메시지 전송 구현
    - 텍스트 입력창 + send 버튼에서 녹음버튼으로 변환
    - 정지 버튼 누르면 메시지 전송하고 Gemini 응답받기

--- KoApp-iOS/KoApp-iOS/Chatting/ChattingView.swift
+++ KoApp-iOS/KoApp-iOS/Chatting/ChattingView.swift
@@ -8,10 +8,23 @@
 import SwiftUI
 
 struct ChattingView: View {
-    @State private var textInput = ""
+    /// Gemini와 채팅기능 담당
     @State private var chatService = ChatService()
-    @FocusState private var isFocused: Bool
     
+    /// STT기능 담당
+    @StateObject private var speechViewModel = SpeechViewModel()
+    
+    /// STT로 인식한 텍스트
+    var recognizedText: String {
+        return speechViewModel.recognizedText
+    }
+    
+    /// 녹음 진행 상태
+    var isRecording: Bool {
+        return speechViewModel.isRecording
+    }
+    
+    /// 힌트 보기 버튼 상태
     @State var showHint: Bool = false
 
 
@@ -38,55 +51,63 @@ struct ChattingView: View {
                 }
             })
             
-            
-            VStack(alignment: .leading) {
+            ZStack() {
                 // MARK: - Hint Button
                 Button(action: {
                     print("힌트 보기")
-                    showHint.toggle()
+                    withAnimation {
+                        showHint.toggle()
+                    }
                     // TODO: 힌트 보기 호출하기
                     
                 }, label: {
                     Image("ic_bulb")
                     Text(showHint ? "힌트 숨기기" : "힌트 보기")
+                        .fontWeight(.bold)
                 })
                 .foregroundStyle(Color.white)
+                .padding(15)
                 // TODO: orange-medium으로 색이름 대체
-                .padding()
                 .background(showHint ? Color(red: 0.57, green: 0.59, blue: 0.6) : Color(red: 0.99, green: 0.56, blue: 0.3))
                 .clipShape(RoundedRectangle(cornerRadius: 40))
                 .shadow(color: Color(red: 0.24, green: 0.26, blue: 0.27).opacity(0.12), radius: 4, x: 0, y: 4)
                 .padding(.bottom, 0)
                 .padding(.leading, 10)
+                .frame(maxWidth: .infinity, alignment: .leading)
                 
-                // TODO: TextField -> STT 녹음 버튼으로 전환 필요
-                // MARK: - Input Field
-                HStack {
-                    TextField("메세지를 입력해주세요...", text: $textInput)
-                        .textFieldStyle(.roundedBorder)
-                        .foregroundStyle(.black)
-                        .focused($isFocused)
-                        .disabled(chatService.loadingResponse)
-                        
-                    if chatService.loadingResponse {
-                        //MARK: - Loading indicator
-                        ProgressView()
-                            .tint(.white)
-                            .frame(width: 30)
+                
+                // MARK: 녹음 버튼(&정지 버튼)
+                Button(action: {
+                    speechViewModel.startRecording()
+                    // 정지 버튼 눌렀을 때 메시지 전송
+                    if !isRecording {
+                        print(recognizedText)
+                        sendMessage()
+                    }
+                }) {
+                    if isRecording {
+                        // 녹음 시작했을 때 정지버튼 보여주기
+                        Image(systemName: "square.fill")
+                            .font(.title)
+                            .frame(width: 64, height: 64)
+                            .foregroundStyle(Color.white)
+                            .background(Color(red: 0.99, green: 0.76, blue: 0.09))
+                            .clipShape(Circle())
+                            .shadow(color: .black.opacity(0.2), radius: 8, x: 0, y: 4)
                     } else {
-                        //MARK: - Send Button
-                        Button(action: sendMessage, label: {
-                            Image(systemName: "paperplane.fill")
-                                .padding(.horizontal)
-                                .foregroundStyle(Color.yellow)
-                        })
-                        .frame(width: 30)
-                        .disabled(textInput.isEmpty)
+                        // 녹음중이 아닐 때(시작하기 전), 녹음 버튼 보여주기
+                        Image("ic_mic_36")
+                            .frame(width: 64, height: 64)
+                            .foregroundStyle(Color.white)
+                            .background(Color(red: 0.99, green: 0.76, blue: 0.09))
+                            .clipShape(Circle())
+                            .shadow(color: .black.opacity(0.2), radius: 8, x: 0, y: 4)
                     }
                 }
-                .padding()
-            .background(Color.white)
+                .frame(maxWidth: .infinity, alignment: .center)
+
             }
+            .padding(.bottom, 40)
 
         }
         .foregroundStyle(.white)
@@ -96,9 +117,7 @@ struct ChattingView: View {
                 Color(red: 247/255, green: 249/255, blue: 250/255)
             }
             .ignoresSafeArea()
-            .onTapGesture {
-                isFocused = false
-            }
+
         }
         
         
@@ -108,8 +127,7 @@ struct ChattingView: View {
     //MARK: - Fetch Response
     private func sendMessage() {
         Task {
-            await chatService.sendMessage(message: textInput)
-            textInput.removeAll()
+            await chatService.sendMessage(message: recognizedText)
         }
     }
 

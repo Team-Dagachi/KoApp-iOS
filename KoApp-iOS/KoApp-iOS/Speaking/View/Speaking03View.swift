dcf1c17517c16eb415af627e54b11c259c2a1648
msseock <msseock@gmail.com>
2024-07-14 16:29:24 +0900

    ✨ Feat: Speaking03 UI 구현
    SubTopic에 따라 상황과 대화Tip이 적용되어서 뜨도록 구현

---
+++ KoApp-iOS/KoApp-iOS/Speaking/View/Speaking03View.swift
@@ -0,0 +1,106 @@
+//
+//  Speaking03View.swift
+//  KoApp-iOS
+//
+//  Created by 석민솔 on 7/14/24.
+//
+
+import SwiftUI
+
+struct Speaking03View: View {
+    let subTopic: SpeakingSubTopic
+    
+    var vm: Speaking03ViewModel {
+        Speaking03ViewModel(subTopic: self.subTopic)
+    }
+    
+    var body: some View {
+        ZStack {
+            Color.gray100
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading) {
+                HStack(spacing: 0) {
+                    Image("ic_chevron_left")
+                    
+                    Spacer()
+                    
+                    Text(subTopic.rawValue)
+                        .font(.H2)
+                    
+                    Spacer()
+
+                    NavigationLink {
+                        Speaking04View()
+                    } label: {
+                        Image("ic_history_24")
+                    }
+                }
+                .padding(.vertical, 17)
+                
+                ScrollView {
+                    Text("상황")
+                        .font(.H4)
+                        .foregroundStyle(Color.gray500)
+                        .padding(.top, 16)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    Text(vm.situation)
+                        .font(.subTitle)
+                        .padding(.vertical, 24)
+                        .padding(.horizontal, 16)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(
+                            RoundedRectangle(cornerRadius: 16)
+                                .foregroundStyle(Color.white)
+                        )
+                    
+                    Text("대화 Tip")
+                        .font(.H4)
+                        .foregroundStyle(Color.gray500)
+                        .padding(.top, 30)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    // 대화 tip 리스트
+                    VStack(alignment: .leading, spacing: 0) {
+                        ForEach(Array(vm.speakingTips.enumerated()), id: \.element) { index, tip in
+                            Text(tip)
+                                .font(.H4)
+                                .padding(.vertical, 24)
+                                .padding(.horizontal, 16)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .background(Color.white)
+                            if index < vm.speakingTips.count - 1 {
+                                Divider()
+                            }
+                        }
+                    }
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                    
+                    NavigationLink {
+                        ChattingView()
+                    } label: {
+                        Text("연습하기")
+                            .font(.H3)
+                            .foregroundStyle(Color.white)
+                            .padding(.vertical, 16)
+                            .frame(maxWidth: .infinity)
+                            .background(
+                                RoundedRectangle(cornerRadius: 48)
+                                    .foregroundStyle(Color.main100)
+                            )
+                    }
+                    .padding(.top, 40)
+                }
+
+                
+            }
+            .padding(.horizontal, 16)
+        }
+        
+    }
+}
+
+#Preview {
+    Speaking03View(subTopic: .travel_experience)
+}
---
+++ KoApp-iOS/KoApp-iOS/Speaking/View/Speaking04View.swift
@@ -0,0 +1,18 @@
+//
+//  Speaking04View.swift
+//  KoApp-iOS
+//
+//  Created by 석민솔 on 7/14/24.
+//
+
+import SwiftUI
+
+struct Speaking04View: View {
+    var body: some View {
+        Text("표현집")
+    }
+}
+
+#Preview {
+    Speaking04View()
+}
